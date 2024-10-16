13b80faf54eb9ee38f190cff3c64bd753d1e337d
Luiz Verissimo <130661503+brocasito1@users.noreply.github.com>
2024-10-16 19:19:18 -0300

    Update informacoesGlobais.js

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,4 +1,4 @@
-const url = 'https://raw.githubusercontent.com/victorborbasilva/api/main/dados-globais.json'
+const url = 'https://github.com/brocasito1/API'
 
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
